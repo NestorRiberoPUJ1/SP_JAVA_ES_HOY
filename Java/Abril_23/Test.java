c2701b43f076423c61f1a2d6fc5c4a8c38750160
NestorRiberoPUJ1 <67485144+NestorRiberoPUJ1@users.noreply.github.com>
2024-04-24 05:40:06 -0500

    Create Test.java

---
+++ Java/Abril_23/Test.java
@@ -0,0 +1,9 @@
+package Java.Abril_23;
+public class Test {
+    /* FUNCION PRINCIPAL DEBE SER UNICA EN TODO EL PROYECTO */
+    public static void main(String[] args) {
+        System.out.println("");
+        System.out.println("HOLA A TODOS");
+    }
+
+}
